d9fb3793f6418204eebdb8e89dc6cdc95196cba9
Richard Derryberry <richardtderryberry@gmail.com>
2022-12-25 14:20:12 -0500

    Initial Commit

---
+++ vectors/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  vectors
+//
+//  Created by Richard Derryberry on 12/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
