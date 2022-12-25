122f14bcf928f72953253d14d3501bd2c9470e50
Richard Derryberry <richardtderryberry@gmail.com>
2022-12-25 14:28:16 -0500

    [vectors] First vector test.

--- vectors/main.cpp
+++ vectors/main.cpp
@@ -6,9 +6,18 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    std::vector<int> count (4, 0);
+    count[0] = 7;
+    count[1] = count[0] * 2;
+    count[2]++;
+    count[3] -= 60;
+    
+    for (int i{0}; i < size(count); i++)
+    {
+        std::cout << count[i] << '\n';
+    }
     return 0;
 }
